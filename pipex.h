d60e8aa75fd49d7584a5c30072ad8479cf46346c
chtang <chtang@student.42tokyo.jp>
2024-01-10 03:13:10 +0900

    a

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: chtang <chtang@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 22:22:47 by chtang            #+#    #+#             */
-/*   Updated: 2024/01/10 02:02:58 by chtang           ###   ########.fr       */
+/*   Updated: 2024/01/10 03:12:51 by chtang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ void	print_error(char *str, char *msg);
 char	*path_join(char const *s1, char const *s2);
 void	exit_with_fail(char *s);
 int		not_a_path(char *str);
-char	***parcing(int nb_cmd, char **av, char **env);
-void	do_pipe(char ***cmds, char **env, int n_pipes, int *in_out_file);
+char	***parcing(char **av, char **env);
+void	do_pipe(char ***cmds, char **env, int *in_out_file);
 // #include "test.h"
 
 #endif
